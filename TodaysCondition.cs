326b05aa788a07528fa31f1198a166294e86b9ad
Vladislava <122866844+SleEpyHeDgEhOg6@users.noreply.github.com>
2025-12-26 23:58:45 +0300

    Create TodaysCondition.cs

---
+++ TodaysCondition.cs
@@ -0,0 +1,9 @@
+namespace StockAnalyzer.Models
+{
+    public class TodaysCondition
+    {
+        public int Id { get; set; }
+        public int TickerId { get; set; }
+        public string State { get; set; }
+    }
+}
